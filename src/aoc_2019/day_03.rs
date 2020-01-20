b7792e0dc33a6e899c985a72f3d90742538bddb7
Xavier Villaneau <xvillaneau@gmail.com>
2020-01-19 22:49:05 -0500

    Minor improvements

--- src/aoc_2019/day_03.rs
+++ src/aoc_2019/day_03.rs
@@ -2,8 +2,7 @@ use std::collections::VecDeque;
 
 pub fn main(_puzzle_input: &String) {}
 
-#[derive(Debug, PartialEq)]
-struct Pos(i16, i16);
+type Position = (i16, i16);
 
 #[derive(Debug, PartialEq)]
 enum Direction { Up, Right, Down, Left }
@@ -15,41 +14,43 @@ struct Move {
 }
 
 impl Move {
-    fn from_string(string: &str) -> Option<Move> {
-        if string.len() < 2 { return None };
+    fn parse(string: &str) -> Option<Move> {
         let mut chars = string.chars();
-        let direction = match chars.next().unwrap_or(' ') {
-            'U' => Direction::Up,
-            'R' => Direction::Right,
-            'D' => Direction::Down,
-            'L' => Direction::Left,
-            _   => return None,
+
+        let direction = match chars.next() {
+            Some('U') => Direction::Up,
+            Some('R') => Direction::Right,
+            Some('D') => Direction::Down,
+            Some('L') => Direction::Left,
+            _         => return None,
         };
+
         let steps: String = chars.collect();
         let steps: u16 = match steps.parse() {
             Ok(value) => value,
             Err(_) => return None,
         };
+
         Some(Move { direction, steps })
     }
 }
 
 #[derive(Debug)]
 struct Path {
-    position: Pos,
+    position: Position,
     moves: VecDeque<Move>,
     current_steps: u16,
 }
 
 impl Path {
-    fn from_string(string: &String) -> Path {
+    fn parse(string: &String) -> Path {
         let mut moves = VecDeque::new();
         for mv_str in string.split(",") {
-            if let Some(mv) = Move::from_string(mv_str) {
+            if let Some(mv) = Move::parse(mv_str) {
                 moves.push_back(mv);
             }
         }
-        Path { position: Pos(0, 0), moves, current_steps: 0 }
+        Path { position: (0, 0), moves, current_steps: 0 }
     }
 }
 
@@ -58,7 +59,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_load_path() {
+    fn test_parse_path() {
         let input = String::from("U1,R1,D1,L1");
         let expected = vec![
             Move { direction: Direction::Up, steps: 1 },
@@ -67,10 +68,10 @@ mod tests {
             Move { direction: Direction::Left, steps: 1 },
         ];
 
-        let path = Path::from_string(&input);
+        let path = Path::parse(&input);
         assert_eq!(expected, Vec::from(path.moves));
         assert_eq!(0, path.current_steps);
-        assert_eq!(Pos(0, 0), path.position);
+        assert_eq!((0, 0), path.position);
     }
 }
 
