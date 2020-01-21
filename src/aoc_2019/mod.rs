d09ce262c0607defa54353dd08a2929b41392d5c
Xavier Villaneau <xvillaneau@gmail.com>
2020-01-20 19:02:18 -0500

    Solved part 1 of 2019.04

--- src/aoc_2019/mod.rs
+++ src/aoc_2019/mod.rs
@@ -1,12 +1,14 @@
 mod day_01;
 mod day_02;
 mod day_03;
+mod day_04;
 
 pub fn dispatch(day: u8, puzzle_input: &String) {
     match day {
          1 => day_01::main(&puzzle_input),
          2 => day_02::main(&puzzle_input),
          3 => day_03::main(&puzzle_input),
+         4 => day_04::main(&puzzle_input),
          _ => println!("Day {} not found", day),
     }
 }
